a0642aa05c80d0ae9ccd09cd0760504fc8918c7e
calyr <calyr.software@gmail.com>
2016-07-04 11:13:40 -0400

    init

--- Ioscloud/ViewController.swift
+++ Ioscloud/ViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var txtIsbn: UITextField!
+    @IBOutlet weak var txtResult: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        txtIsbn.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        sincrono(txtIsbn.text!)
+        return true
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pressEnter(sender: UITextField) {
+    print("Imprimir el valor \(txtIsbn.text)")
+    }
+    
+    func sincrono(isbn:String){
+        
+        let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"+isbn
+        let url = NSURL(string: urls)
+        let datos:NSData? = NSData(contentsOfURL: url!)
+        let texto = NSString( data: datos!, encoding: NSUTF8StringEncoding)
+        txtResult.text = String(texto)
+    }
 
 }
 
